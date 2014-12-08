05d8547cbdca57bf7a2049328f8371370e5059f9
Dan Racanelli <accounts@attnspan.com>
2014-12-07 22:49:09 -0800

    Near final pass before kicking to GH  * Implemented --missing option  * Adding README

---
+++ README.md
@@ -0,0 +1,54 @@
+# npm-update-outdated
+
+If you're ever thinking that the [npm outdated] command should have an _--all_ option, then this is the module for you.
+
+## Installation
+
+```
+npm install -g npm-update-outdated
+```
+
+## Usage
+
+```bash
+$ cd /path/to/your/node/project
+$ npm-update-outdated [options]
+```
+
+## Options
+
+```bash
+$ npm-update-outdated --help
+
+  Usage: npm_update_outdated [options]
+
+  Options:
+
+    -h, --help               output usage information
+    -V, --version            output the version number
+    --filter <string|regex>  filter packages to be updated by string or regex
+    --missing                install missing modules
+```
+
+The MIT License
+===============
+
+Copyright (c) 2014 Dan Racanelli
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
--- bin/npm_update_outdated.js
+++ bin/npm_update_outdated.js
@@ -1,36 +1,59 @@
-#!/usr/local/bin/node
+#!/usr/bin/env node
 
-process.stdin.setEncoding('utf8');
+'use strict';
 
-var data = '';
-process.stdin.on('readable', function() {
-    var chunk = process.stdin.read();
-    if (chunk !== null) {
-        data += chunk;
-    }
-});
-
-process.stdin.on('end', function() {
-    main(data);
-});
+var commander = require('commander'),
+    exec      = require('child_process').exec,
+    when      = require('when'),
+    nodefn    = require('when/node'),
+    pExec     = nodefn.lift(exec),
+    pkg       = require('../package.json');
 
 var NPMUdateOutdated = require('../lib/NPMUpdateOutdated.js');
+var updater = new NPMUdateOutdated();
+
+commander.version(pkg.version)
+    .option('--filter <string|regex>', 'filter packages to be updated by string or regex', updater.SetFilter.bind(updater))
+    //.option('-y', 'disable confirmation prompts and auto-update modules', updater.SetAutoUpdate.bind(updater, true))
+    .option('--missing', 'install missing modules', updater.SetInstallMissing.bind(updater, true))
+    .usage('[options]')
+    .parse(process.argv);
+
+pExec('npm outdated --parseable')
+    .then(main)
+    .catch(function(err)
+    {
+        console.error('npm outdated command failed');
+        console.error(err);
+        process.exit(1);
+    });
 
 function main(data)
 {
-    var updater = new NPMUdateOutdated();
-    updater.Load(data);
+    if (data && data.length)
+    {
+        updater.Load(data[0]);
 
-    var outdated = updater.GetOutdated();
+        var outdated = updater.GetOutdated();
 
-    updater.UpdateOutdated(outdated);
+        updater.UpdateOutdated(outdated);
 
-    updater.on('end', function()
+        updater.on('outdated_end', function()
+        {
+            updater.InstallMissing();
+        })
+        updater.on('missing_end', function()
+        {
+            console.log('All done!');
+            process.exit(0);
+        })
+        .on('error', function(err)
+        {
+            process.exit(1);
+        });
+    }
+    else
     {
         process.exit(0);
-    })
-    .on('error', function(err)
-    {
-        process.exit(1);
-    });
+    }
 }
